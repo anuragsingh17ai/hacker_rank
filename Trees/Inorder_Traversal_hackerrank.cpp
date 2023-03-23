0db90fd76b4dffe852d3d32d51e9cee8c776b122
Anurag Singh <78840945+anuragsingh17ai@users.noreply.github.com>
2023-03-23 11:54:34 +0530

    Create Inorder_Traversal_hackerrank.cpp

---
+++ Trees/Inorder_Traversal_hackerrank.cpp
@@ -0,0 +1,8 @@
+    void inOrder(Node *root) {
+        if(!root) return;
+        
+            inOrder(root->left);
+            cout<<root->data<<" ";
+            inOrder(root->right);
+            
+    }
